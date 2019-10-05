dfcc7357f99c6a94a6988fc0e5aa49fc9c16c4a6
Joseph <admin@jweissig.com>
2019-10-04 18:57:55 -0700

    Planets and solar system

---
+++ CRT/SolarSystem/SolarSystem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRT.SolarSystem
+{
+    public class SolarSystem
+    {
+        List<Planet> planets;
+
+        public void Init()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Planet newPlanet = new Planet(i);
+                planets.Add(newPlanet);
+            }
+        }
+
+        public void Update()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+
+            }
+        }
+    }
+}
